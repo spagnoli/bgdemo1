1323d405229f07e03e8f326acb59d950c70cbdbf
Chauncey Thorn <chaunceyt@gmail.com>
2017-10-05 20:49:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
               </tr>
               <tr>
                 <td>Application Version</td>
-                <td>v2.1.1</td>
+                <td>v3.0.0</td>
               </tr>              
             </tbody>
           </table>
@@ -94,7 +94,7 @@
         <div class="col-md-6 col-md-offset-3">
           <h2>Application Example</h2>
           <p>
-            <img src="redrose.jpeg">
+            <img src="greenrose.jpeg">
           </p>
         </div>
       </div>
